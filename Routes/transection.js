b89af914653eef389007756288fd1e7a8185827b
khaleeque <kmem98@gmail.com>
2023-03-04 15:12:56 +0500

    code updated

--- Routes/transection.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const auth = require('../middleware/jwt');
-const {addBalance} = require('../controllers/walletController');
-
-const router = express.Router();
-
-router.route('/add/balance').post(auth,addBalance);
-
-module.exports = router;
--- middleware/auth.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("express-jwt");
-const secret = process.env.JWT_SECRET_KEY;
-
-const authenticate = jwt({
-	secret: secret
-});
-
-module.exports = authenticate;
